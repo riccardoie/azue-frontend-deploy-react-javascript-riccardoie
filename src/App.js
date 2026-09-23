55937e404f71a51a92f25e968e646bc2d367a073
Riccardo <riccardo.ieva@no.experis.com>
2026-09-23 11:23:54 +0200

    feat: testing update.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}, IT WORKED!!!!</div>;
 }
 
 export default App;
